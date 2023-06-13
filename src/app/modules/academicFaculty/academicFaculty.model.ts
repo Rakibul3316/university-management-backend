541a7f5ae0f540fd7a055d8c070cac070802fdf3
Rakibul3316 <service.rakibul4341@gmail.com>
2023-06-13 20:53:02 +0600

    [DONE] Academic Faculty CRUD

---
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose';
+import {
+  AcademicFacultyModel,
+  IAcademicFaculty,
+} from './academicFaculty.interface';
+
+const academicFacultySchema = new Schema<IAcademicFaculty>(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const AcademicFaculty = model<IAcademicFaculty, AcademicFacultyModel>(
+  'academicFaculty',
+  academicFacultySchema,
+  'academicFaculty'
+);
